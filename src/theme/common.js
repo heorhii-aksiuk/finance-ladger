3a0ea284c7e4cecf562c0f026c5341046b9a9c46
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-07-26 15:48:01 +0300

    init, icons, manifest, title, discription and theme mvp object

---
+++ src/theme/common.js
@@ -0,0 +1,21 @@
+const common = {
+  fontFamily: "'Open Sans', sans-serif",
+  color: {
+    accent: '#28A745',
+    hoverAccent: '#25D64D',
+
+    mainText: '#333333',
+    secondaryText: '#FFFFFF',
+    heroText: '#E9E9E9',
+    placeholderText: '#808080',
+
+    mainBackground: '#FFFFFF',
+    blogBackground: '#0284D0',
+    formBackground: '#F4F4F4',
+    footerBackground: '#333333',
+
+    buttonBorder: '#FFFFFF',
+  },
+}
+
+export default common
